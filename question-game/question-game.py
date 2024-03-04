b2538a0d42b58e07520455261e2147a29f46a770
David Galstyan <Galstyand.25@gmail.com>
2024-03-04 16:09:13 +0400

    Updated question game

--- question-game/question-game.py
+++ question-game/question-game.py
@@ -1,14 +1,51 @@
-question_file = open('question-game/questions.txt').readlines()
-top_file = open('question-game/top.txt', 'r+')
-
-# nickname = input('Enter your nickname: ')
+import random
 
 
-def start_quest(questions):
-    md = {}
-    for line in questions:
-        line = line.split('? ')
-        md[line[0]] = line[1]
+def quest_read():
+    return open('question-game/questions.txt').readlines()
 
+
+def top_open():
+    return open('question-game/top.txt', 'a')
+
+
+def top_read():
+    return open('question-game/top.txt', 'r').readlines()
+
+name = input('Enter your name: ')
+print('Welcome', name)
+print()
+
+random_nums = []
+while len(random_nums) < 10:
+    i = random.randint(0,len(quest_read()) - 1)
+    if i not in random_nums:
+        random_nums.append(i)
+
+questions_list = []
+for i in random_nums:
+    questions_list.append(quest_read()[i][:-1])
+
+questions_dict = {}
+for question in questions_list:
+    quest,answer = question.split('? ')
+    questions_dict[quest] = answer.split(',')
     
-print(start_quest(question_file))
+count = 0
+
+for quest,answer in questions_dict.items():
+    print(quest + '?')
+    correct = answer[0]
+    random.shuffle(answer)
+    for el in answer:
+        print(el)
+    answer = input('Enter an answer: ')
+    if answer.lower() == correct.lower():
+        print('Correct!')
+        count += 1
+    else:
+        print("Wrong.The answer is", correct)
+
+count = 0
+top_open().write(f'{name}: {count}\n')
+top_open().close()
